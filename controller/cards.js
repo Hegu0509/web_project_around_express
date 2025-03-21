18611adf8450f86317d34ed4cbc7bcb759de1d29
Gustavo Morales <hectorgustavomorales@gmail.com>
2025-03-20 19:21:54 -0500

    Se elimina console log de codigo

--- controller/cards.js
+++ controller/cards.js
@@ -19,7 +19,6 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  console.log(req.user._id);
   const owner = req.user._id;
   Card.create({ name, link, owner })
     .then((card) => {
